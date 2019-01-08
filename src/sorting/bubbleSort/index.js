1c857a735ccd10ee6f18db1d5432a7aa07f061af
Liangxiaojun <fatalxiao@163.com>
2019-01-08 22:38:50 +0800

    update bubbleSort

--- src/sorting/bubbleSort/index.js
+++ src/sorting/bubbleSort/index.js
@@ -1,8 +1,4 @@
-function sort(arr) {
-
-    if (!arr || arr.length < 1) {
-        return arr;
-    }
+function sort1(arr) {
 
     for (let i = 0, len = arr.length; i < len - 1; i++) {
         for (let j = i + 1; j < len; j++) {
@@ -16,4 +12,19 @@ function sort(arr) {
 
 }
 
-sort([5, 2, 6, 68, 3, 2, 1]);
+function sort2(arr) {
+
+    for (let i = 0, len = arr.length; i < len - 1; i++) {
+        for (let j = 0; j < len - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+    }
+
+    return arr;
+
+}
+
+sort1([5, 2, 6, 68, 3, 2, 1]);
+sort2([5, 2, 6, 68, 3, 2, 1]);
